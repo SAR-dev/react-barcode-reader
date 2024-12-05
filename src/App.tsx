c66e645264ec426884216d6a0f63b1e91c8c989d
SAR-dev <sayedarrafi.bd@gmail.com>
2024-12-05 11:37:16 +0900

    up

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import useScanDetection from './useScanDetection';
 function App() {
   const [value, setValue] = useState<string | undefined>('');
   const inputRef = useRef<HTMLInputElement>(null);
+  
+  const [keys, setKeys] = useState<string[]>([])
 
   const handleScanComplete = (e: string) => {
     if (inputRef.current) {
@@ -27,12 +29,14 @@ function App() {
         className="border border-gray-500 rounded w-64 py-1 px-2"
         ref={inputRef}
         onKeyDown={(event) => {
+          setKeys([...keys, event.key])
           if (event.key === 'Enter') {
             setValue(inputRef.current?.value || '');
           }
         }}
       />
       <div>✍: {value}</div>
+      <div>Keys: {keys.join(", ")}</div>
     </div>
   );
 }
