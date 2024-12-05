2c613eaa0f351f8d950266e7cbb4bfd8b935a51b
SAR-dev <sayedarrafi.bd@gmail.com>
2024-12-05 11:52:11 +0900

    up

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const handleScanComplete = (e: string) => {
     if (inputRef.current) {
       const scannedValue = e.toUpperCase();
-      alert("Scanned: " + scannedValue)
+      // alert("Scanned: " + scannedValue)
       inputRef.current.value = scannedValue;
       setValue(scannedValue);
     }
