c8fc67a708b1aa261166378b5b335447ddca0290
SAR-dev <git@stackblitz.com>
2024-12-05 11:29:32 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react';
+import useScanDetection from './useScanDetection';
+
+function App() {
+  const [value, setValue] = useState<string | undefined>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleScanComplete = (e: string) => {
+    if (inputRef.current) {
+      const scannedValue = e.toUpperCase();
+      inputRef.current.value = scannedValue;
+      setValue(scannedValue);
+    }
+  };
+
+  useScanDetection({
+    onComplete: handleScanComplete,
+    minLength: 7,
+  });
+
+  return (
+    <div className="m-5 flex flex-col gap-5">
+      <div>Scanner Input</div>
+      <input
+        type="text"
+        className="border border-gray-500 rounded w-64 py-1 px-2"
+        ref={inputRef}
+        onKeyDown={(event) => {
+          if (event.key === 'Enter') {
+            setValue(inputRef.current?.value || '');
+          }
+        }}
+      />
+      <div>✍: {value}</div>
+    </div>
+  );
+}
+
+export default App;
